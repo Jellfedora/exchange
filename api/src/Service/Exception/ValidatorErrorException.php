21503b53750ca1c298f702f193e5668befc9225d
Jellfedora <julienlecointe@live.fr>
2021-04-09 18:06:54 +0200

    Added Validator error exception

---
+++ api/src/Service/Exception/ValidatorErrorException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Service\Exception;
+
+class ValidatorErrorException extends \Exception
+{
+    private $errors;
+
+    /**
+     * Get the value of errors
+     */ 
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Set the value of errors
+     *
+     * @return  self
+     */ 
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
+
+        return $this;
+    }
+}
