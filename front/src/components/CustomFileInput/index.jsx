16fb561d36cc97bb2342d5faeac31b51bba9784a
Jellfedora <julienlecointe@live.fr>
2021-04-12 18:37:24 +0200

    Added resize avatar when edit it

--- front/src/components/CustomFileInput/index.jsx
+++ front/src/components/CustomFileInput/index.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Resizer from "react-image-file-resizer";
 
 const CustomeFileInput = (props) => {
+  const onChange = async (e) => {
+    // console.log(e);
+    props.goAvatarSpinner();
+    try {
+      const file = e;
+      const image = await resizeFile(file);
+      // console.log(image);
+      props.submitEditAvatar(image);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const resizeFile = (file) =>
+    new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        400,
+        400,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        "file"
+      );
+    });
   return (
     <div>
       <input
@@ -9,7 +38,8 @@ const CustomeFileInput = (props) => {
         id="avatar"
         name="avatar"
         accept="image/png, image/jpeg"
-        onChange={(e) => props.submitEditAvatar(e.target.files[0])}
+        // onChange={(e) => props.submitEditAvatar(e.target.files[0])}
+        onChange={(e) => onChange(e.target.files[0])}
       />
       <label htmlFor="avatar">
         <FontAwesomeIcon icon="camera" size="2x" />
